dbba6fa5d26db498b1d4937ada068ba1aa7b0773
Andre <aburte@senegalsoftware.com>
2021-05-25 15:54:38 -0400

    Test other API in XTRM.php file

--- scripts/XTRM.php
+++ scripts/XTRM.php
@@ -6,9 +6,9 @@
      * Scripts for testing XTRM API Calls.
      */
 
-     $name = htmlspecialchars($_POST['username']);
-     $email = htmlspecialchars($_POST['useremail']);
-     $pass = htmlspecialchars($_POST['useremail']); 
+     $name = filter_input(INPUT_POST,'username');
+     $email = filter_input(INPUT_POST,'useremail');
+     $pass = filter_input(INPUT_POST,'userpass'); 
 
      echo $name."<br>".$email."<br>".$pass;
 
@@ -16,11 +16,6 @@
     function getAuthToken()
     {
       $credentials = ['grant_type' => 'password','client_id' => '1930815_API_User', 'client_secret' => "TrXSmMCkrGTq2UVsOYMloiPmpwdvIkVrE56DJAkOBkg="];
-    
-      $otherThing = [
-        'email' => "isaaclmelton@gmail.com",
-        'password' => "SecurePass12!!"
-      ];
     
       $curl = curl_init();
     
@@ -59,3 +54,42 @@
     getAuthToken();
     */
 
+
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "https://free-nba.p.rapidapi.com/players?per_page=25&page=0",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "x-rapidapi-host: free-nba.p.rapidapi.com",
+            "x-rapidapi-key: 4f11486c4emshd10f9888549fceep138d5ejsn082ca18ccc1b"
+        ],
+    ]);
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+
+    $response = json_decode($response,true);
+    
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        foreach($response as $players){
+            foreach($players as $player){
+                foreach($player as $key => $val){
+                    if(!empty($key)){
+                        echo $key ."=". $val ."<br>";
+                    }
+                }
+                echo "<br>";
+            }
+        }
+    }
