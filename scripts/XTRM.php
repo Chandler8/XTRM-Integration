c7e9e29f01cb65a20ed77495834f92552383d6f6
Andre <aburte@senegalsoftware.com>
2021-05-28 10:05:12 -0400

    Update XTRM file

--- scripts/XTRM.php
+++ scripts/XTRM.php
@@ -13,26 +13,15 @@
      echo $name."<br>".$email."<br>".$pass."<br><br>";
 */
 
-     function getAuthToken()
-    {
-      //$credentials = ['grant_type' => 'password','client_id' => '1930815_API_User', 'client_secret' => "TrXSmMCkrGTq2UVsOYMloiPmpwdvIkVrE56DJAkOBkg="];
-    
-      $curl = curl_init();
-    
-      //$jsonThing = json_encode($credentials);
-/*
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => "http://xapisandbox.xtrm.com/oAuth/token",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_SSL_VERIFYPEER => 0,
-        CURLOPT_FOLLOWLOCATION => false,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => $jsonThing,*/
+    $auth_str = 'grant_type=password&client_id=1930815_API_User&client_secret=TrXSmMCkrGTq2UVsOYMloiPmpwdvIkVrE56DJAkOBkg=';
+    $refresh_str = 'grant_type=password&client_id=1930815_API_User&client_secret=TrXSmMCkrGTq2UVsOYMloiPmpwdvIkVrE56DJAkOBkg=&refresh_token=ad7e1b4cfe904d88abbcc2fe70828fb9';
 
+    function getAuthToken()
+    {    
+        $curl = curl_init();
+    
+        global $auth_str;
+        
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://xapisandbox.xtrm.com/oAuth/token',
             CURLOPT_RETURNTRANSFER => true,
@@ -42,66 +31,65 @@
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => 'grant_type=password&client_id=1930815_API_User&client_secret=TrXSmMCkrGTq2UVsOYMloiPmpwdvIkVrE56DJAkOBkg=',
-        CURLOPT_HTTPHEADER => array(
-            "Content-Type: application/x-www-form-urlencoded"
-        ),
-      ));
+            CURLOPT_POSTFIELDS => $auth_str,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/x-www-form-urlencoded"
+            ),
+        ));
     
-      $response = curl_exec($curl);
-      $err = curl_error($curl);
-    
-      curl_close($curl);
-    
-      $token = json_decode($response, true);
-    
-      if ($err) {
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        $token = json_decode($response, true);
+
+        if ($err) {
         echo "cURL Error #:" . $err;
-      } else {
-        print_r($token);
-      }
-    }
-
-    getAuthToken();
-    
-
-/*
-    $curl = curl_init();
-    
-    curl_setopt_array($curl, [
-        CURLOPT_URL => "https://free-nba.p.rapidapi.com/players?per_page=25&page=0",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_HTTPHEADER => [
-            "x-rapidapi-host: free-nba.p.rapidapi.com",
-            "x-rapidapi-key: 4f11486c4emshd10f9888549fceep138d5ejsn082ca18ccc1b"
-        ],
-    ]);
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-
-    $response = json_decode($response,true);
-    
-    if ($err) {
-        echo "cURL Error #:" . $err;
-    } else {
-        foreach($response as $players){
-            foreach($players as $player){
-                foreach($player as $key => $val){
-                    if(!empty($key)){
-                        echo $key ."=". $val ."<br>";
-                    }
-                }
-                echo "<br>";
+        } else {
+            foreach($token as $key=>$val){
+                echo $key.": ".$val."<br><br>";
             }
         }
     }
-    */
+
+    //getAuthToken();
+    
+    function refreshAuthToken()
+    {
+        $curl = curl_init();
+
+        global $refresh_str;
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://xapisandbox.xtrm.com/oAuth/token',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $refresh_str,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/x-www-form-urlencoded"
+            ),
+        ));
+    
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        $token = json_decode($response, true);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+            foreach($token as $key=>$val){
+                echo $key.": ".$val."<br><br>";
+            }
+        }
+    }
+
+    refreshAuthToken();
